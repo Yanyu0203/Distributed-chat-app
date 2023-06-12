afb6fab8cf2b4f35f50c9d7beef6848f852d6835
Yanyu0203 <132418583+Yanyu0203@users.noreply.github.com>
2023-06-12 20:04:07 +0800

    backend

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/Yanyu0203/realtimechat-go-react
+
+go 1.20
+
+require github.com/gorilla/websocket v1.5.0 // indirect
---
+++ backend/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Yanyu0203/realtimechat-go-react/pkg/websocket"
+)
+
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+V\n", err)
+	}
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRputes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("Real Time Distributed Chat v0.0.1")
+	setupRputes()
+	http.ListenAndServe(":8080", nil)
+}
